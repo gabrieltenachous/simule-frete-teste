af4bb21de0a0e8406eb98316194fcfabf141c51e
root <gabrielbromo@outlook.com>
2024-01-10 10:17:54 -0300

    install laravel breeze and tawindcss

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('dashboard');
+
+Route::resource('produto',ProductController::class);
+Route::resource('fornecedor',ProductController::class);
